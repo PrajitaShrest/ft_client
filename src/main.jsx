942bd21ba774e9fc8752c6b621d93f563132da0f
PrajitaShrest <163495834+PrajitaShrest@users.noreply.github.com>
2025-03-15 18:28:51 +1100

    workshop update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { createContext, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// TODO: 1. Include Bootstrap CSS for styling
+//TODO: 1. Include Bootstrap CSS for styling
 // Import Bootstrap's CSS file in the entry point to apply default styles.
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
